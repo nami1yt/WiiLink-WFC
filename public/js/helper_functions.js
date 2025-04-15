e9720ef98d09aa9b22112a4eb2e3d46b7f6f066a
Alex <basurillodesgastorito@gmail.com>
2025-04-15 23:34:00 +0200

    Update helper_functions.js

--- public/js/helper_functions.js
+++ public/js/helper_functions.js
@@ -364,10 +364,10 @@ export function getController(controller) {
       case "mii":
         break;
       default:
-        return '<img src="/img/controllers/wiimote.svg" style="margin-right:15px; filter:brightness(1000); transform:translate(-150%, 0px);" height="60px">';
+        return '<img src="/img/controllers/nintendods.svg" style="margin-right:15px; filter:brightness(1000); transform:translate(-150%, 0px);" height="60px">';
     }
   } else {
-    return '<img src="/img/controllers/wiimote.svg" style="margin-right:15px; filter:brightness(1000); transform:translate(-150%, 0px);" height="60px">';
+    return '<img src="/img/controllers/nintendods.svg" style="margin-right:15px; filter:brightness(1000); transform:translate(-150%, 0px);" height="60px">';
   }
 }
 
